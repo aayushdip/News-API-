8aa4425f194212ebc4e17c6cba1f9420224fdb9f
Aayush Dip Giri <aayush.giri@codavatar.tech>
2023-08-01 12:32:18 +0545

    feat: database connection using Edgedb

---
+++ main.py
@@ -0,0 +1,21 @@
+# app/main.py
+from __future__ import annotations
+
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from routers import user
+
+app = FastAPI()
+
+# Set all CORS enabled origins.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(user.router)
---
+++ routers/user.py
@@ -0,0 +1,34 @@
+from fastapi import Depends, APIRouter, status, HTTPException, Query
+from http import HTTPStatus
+from queries import (
+    create_user_async_edgeql as create_user_qry,
+    get_subscriber_list_async_edgeql as get_user_subscriber_qry,
+    get_user_by_username_async_edgeql as get_user_with_username_qry,
+    get_users_async_edgeql as get_user_all_list_qry,
+)
+from schemas.user_schema import UserCreate, UserInDB, UserRead
+from hashing_password import hash_password
+import edgedb
+
+router = APIRouter()
+client = edgedb.create_async_client()
+
+
+@router.post("/users", status_code=status.HTTP_201_CREATED)
+async def post_user(user: UserCreate) -> create_user_qry.CreateUserResult:
+    try:
+        created_user = await create_user_qry.create_user(
+            client,
+            name=user.name,
+            email=user.email,
+            username=user.username,
+            admin=user.admin,
+            subscriber=user.subscriber,
+            hashed_password=hash_password(user.hashed_password),
+        )
+    except edgedb.errors.ConstraintViolationError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"error": f"Username '{user.name}' already exists."},
+        )
+    return created_user
